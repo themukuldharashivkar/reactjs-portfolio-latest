d0c764d439ec11d99817817232a4081a1304cdb5
Mukul <mukul.dharashivkar99@gmail.com>
2023-03-12 11:32:06 +0530

    Change CSS in Section and About

--- src/components/About.js
+++ src/components/About.js
@@ -7,14 +7,14 @@ const About = () => {
     <Section title="About Me" subtitle="Full Stack Developer">
       <div className="mx-auto lg:mx-10  md:mx-6 grid md:grid-cols-2 items-center justify-between md:justify-evenly">
         <div className="about-info mx-auto justify-between">
-          <p className="md:max-w-lg font-extralight pb-5 justify-center ">
+          <p className="md:max-w-lg font-light text-gray-500 pb-5 justify-center ">
             Hi, I am Mukul Rajendra Dharashivkar and everyone calls me Mukul. I am a Full Stack Web Developer. I build beautiful websites with React and Tailwind CSS.
           </p>
-          <p className="md:max-w-lg font-extralight pb-5 justify-center">
+          <p className="md:max-w-lg font-light text-gray-500 pb-5 justify-center">
             I have recently completed my Bachelor of Engineering in Computer Engineering from Progressive Education Society's Modern College of Engineering, Pune.
           </p>
-          <p className="md:max-w-lg font-extralight pb-5 justify-center">I am proficient in ReactJS, Spring, Spring Boot, Bootstrap, Flask, Tailwind CSS and many more. In backend I can work in Spring, Spring Boot, MySQL, Flask</p>
-          <p className="md:max-w-lg font-extralight pb-5 justify-center">
+          <p className="md:max-w-lg font-light text-gray-500 pb-5 justify-center">I am proficient in ReactJS, Spring, Spring Boot, Bootstrap, Flask, Tailwind CSS and many more. In backend I can work in Spring, Spring Boot, MySQL, Flask</p>
+          <p className="md:max-w-lg font-light text-gray-500 pb-5 justify-center">
             In my spare time I play cricket and read blogs.
           </p>
         </div>
--- src/components/common/Section.js
+++ src/components/common/Section.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Section = ({title, subtitle, children}) => {
   return (
     <section className="min-h-fit flex flex-col justify-start items-center py-16 px-5 text-center">
-        <h3 className="py-3 text-3xl lg:text-5xl">{title}</h3>
+        <h3 className="py-3 text-3xl lg:text-5xl font-semibold text-orange-500">{title}</h3>
         <p className="max-w-xl font-light text-gray-500 mb-10 text-sm md:text-base">{subtitle}</p>
         {children}
     </section>
