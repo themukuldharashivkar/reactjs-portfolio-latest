1dc6322f4a3005fdbe88099f7cbc94c93e6f2a4f
Mukul <mukul.dharashivkar99@gmail.com>
2023-03-12 11:41:04 +0530

    Change CSS in Section and About

--- src/components/About.js
+++ src/components/About.js
@@ -6,15 +6,15 @@ const About = () => {
   return (
     <Section title="About Me" subtitle="Full Stack Developer">
       <div className="mx-auto lg:mx-10  md:mx-6 grid md:grid-cols-2 items-center justify-between md:justify-evenly">
-        <div className="about-info mx-auto justify-between">
-          <p className="md:max-w-lg font-light text-gray-500 pb-5 justify-center ">
+        <div className="about-info mx-auto justify-between text-left">
+          <p className="md:max-w-lg font-light pb-5 justify-center ">
             Hi, I am Mukul Rajendra Dharashivkar and everyone calls me Mukul. I am a Full Stack Web Developer. I build beautiful websites with React and Tailwind CSS.
           </p>
-          <p className="md:max-w-lg font-light text-gray-500 pb-5 justify-center">
+          <p className="md:max-w-lg font-light pb-5 justify-center">
             I have recently completed my Bachelor of Engineering in Computer Engineering from Progressive Education Society's Modern College of Engineering, Pune.
           </p>
-          <p className="md:max-w-lg font-light text-gray-500 pb-5 justify-center">I am proficient in ReactJS, Spring, Spring Boot, Bootstrap, Flask, Tailwind CSS and many more. In backend I can work in Spring, Spring Boot, MySQL, Flask</p>
-          <p className="md:max-w-lg font-light text-gray-500 pb-5 justify-center">
+          <p className="md:max-w-lg font-light pb-5 justify-center">I am proficient in ReactJS, Spring, Spring Boot, Bootstrap, Flask, Tailwind CSS and many more. In backend I can work in Spring, Spring Boot, MySQL, Flask</p>
+          <p className="md:max-w-lg font-light pb-5 justify-center">
             In my spare time I play cricket and read blogs.
           </p>
         </div>
@@ -23,7 +23,7 @@ const About = () => {
           <img
             src={AboutImg}
             alt="coding illustration"
-            className="lgw-[80%]  bg-gradient-to-b from-orange-500 rounded-xl pt-5"
+            className="lgw-[80%]  bg-gradient-to-b from-blue-600 rounded-xl pt-5"
           />
         </div>
       </div>
